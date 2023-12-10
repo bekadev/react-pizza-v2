641ca5e513ed18e9c160235ecb5fe2af078d25a7
Bekzat Kyzyrbayev <bekapng@gmail.com>
2023-12-10 16:01:58 +0600

    updated components

--- src/App.js
+++ src/App.js
@@ -4,24 +4,26 @@ import Categories from "./components/Categories";
 import Sort from "./components/Sort";
 import PizzaBlock from "./components/PizzaBlock";
 
+import pizzas from './assets/pizzas.json'
+
 function App() {
-    return (
-        <div className="wrapper">
-            <Header/>
-            <div className="content">
-                <div className="container">
-                    <div className="content__top">
-                        <Categories/>
-                        <Sort/>
-                    </div>
-                    <h2 className="content__title">Все пиццы</h2>
-                    <div className="content__items">
-                        <PizzaBlock />
-                    </div>
-                </div>
-            </div>
+  return (<div className="wrapper">
+    <Header/>
+    <div className="content">
+      <div className="container">
+        <div className="content__top">
+          <Categories/>
+          <Sort/>
         </div>
-    );
+        <h2 className="content__title">Все пиццы</h2>
+        <div className="content__items">
+          {pizzas.map(obj => {
+            return <PizzaBlock {...obj} key={obj.id}/>
+          })}
+        </div>
+      </div>
+    </div>
+  </div>);
 }
 
 export default App;
